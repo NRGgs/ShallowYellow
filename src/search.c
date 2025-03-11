d2717b39834e8f9cbc1cc35d2c25bbcbff0230e1
Mika Schipper <mschippe@student.codam.nl>
2025-03-11 20:58:07 +0100

    fix alphabeta stuff with some help from Dev thx

--- src/search.c
+++ src/search.c
@@ -10,13 +10,17 @@ int *evaluate(const struct position *pos, struct move move, int **table, int tot
 struct search_result minimax(const struct position *pos, const struct position *og, struct move *move, int depth, int **table,
 							int alpha, int beta, int total_value[2]) {
 	struct search_result result;
+	int valuecpy[2];
+	valuecpy[0] = total_value[0];
+	valuecpy[1] = total_value[1];
+	int is_maxing = depth % 2;
 
-	result.score = -1000000;
+	result.score = (is_maxing) ? -1000000 : 1000000;
 
 	if (depth == 0) {
 		/* we have reached our search depth, so evaluate the position.       */
-		total_value = evaluate(og, *move, table, total_value);
-		result.score = total_value[pos->side_to_move] - total_value[1 - pos->side_to_move];
+		evaluate(og, *move, table, valuecpy);
+		result.score = valuecpy[pos->side_to_move] - valuecpy[1 - pos->side_to_move];
 	} else {
 		struct move moves[MAX_MOVES];
 		size_t count = generate_legal_moves(pos, moves);
@@ -32,24 +36,31 @@ struct search_result minimax(const struct position *pos, const struct position *
 
 			/* minimax is called recursively. this call returns the score of */
 			/* the opponent, so we must negate it to get our score.          */
-			score = -minimax(&copy, pos, &moves[index], depth - 1, table, -beta, -alpha, total_value).score;
+			score = minimax(&copy, pos, &moves[index], depth - 1, table, alpha, beta, valuecpy).score;
 
 			/* update the best move if we found a better one.                */
-			if (score > result.score) {
-				result.move = moves[index];
-				result.score = score;
-				if (score > alpha)
+			if (is_maxing) {
+				if (score > result.score)
 				{
-					alpha = score;
-					printf("------UPDATING-alpha:%d\n", alpha);
+					result.move = moves[index];
+					result.score = score;
 				}
+				alpha = (score > alpha) ? score : alpha;
 			}
-			if (score >= beta)
-				return (result);
+			else {
+				if (score < result.score)
+				{
+					result.move = moves[index];
+					result.score = score;
+				}
+				beta = (score < beta) ? score : beta;
+			}
+			if (alpha >= beta)
+				break;
 		}
 	}
-	printf("----------%d----------\n", result.score);
-	printf("-----beta: %d -----alpha: %d -----\n\n", beta, alpha);
+	// printf("----------%d----------\n", result.score);
+	// printf("-----beta: %d -----alpha: %d -----\n\n", beta, alpha);
 	return result;
 }
 
@@ -69,7 +80,7 @@ struct move search(const struct search_info *info, int **table) {
 	}
 	printf("-----score: %d-----\n\n\n", score[info->pos->side_to_move] - score[1 - info->pos->side_to_move]);
 	// return minimax(info->pos, NULL, NULL, 8, table, alpha, beta, score).move;
-	struct search_result result = minimax(info->pos, NULL, NULL, 4, table, alpha, beta, score);
+	struct search_result result = minimax(info->pos, NULL, NULL, 5, table, alpha, beta, score);
 	printf("----------%d----------\n\n", result.score);
 	return (result.move);
 }
