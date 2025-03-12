1e695fbcba253e80a824706aef5bf07c406fd006
Mika Schipper <mschippe@student.codam.nl>
2025-03-12 16:10:18 +0100

    did something

--- src/zobrist.c
+++ src/zobrist.c
@@ -1,5 +1,8 @@
 #include <stdlib.h>
 #include "../include/zobrist.h"
+#include "../include/bitboards.h"
+
+void	init_board(t_board *board);
 
 long random_long() {
 	unsigned int lower = rand();
@@ -20,8 +23,15 @@ void fill_zobrist_hashes(zob_hashes *hashes)
 	hashes->side_hashes[1] = random_long();
 }
 
+long init_gamestate(zob_hashes *hashes, t_board *board)
+{
+
+}
+
 int main() {
+	t_board board;
 	zob_hashes hashes;
+	init_board(&board);
 	fill_zobrist_hashes(&hashes);
 	for (int i = 0; i < 64 * 6 * 2; i++)
 		printf("%ld\n", hashes.piece_hashes[i]);
